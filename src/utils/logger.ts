6e41fd4318645456a83fe3b3573d4c268fdd3e15
Lucas Prieto <lucasprieto12@gmail.com>
2023-02-15 13:52:21 -0300

    add vercel config

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,7 +2,7 @@ import { existsSync, mkdirSync } from 'fs'
 import { join } from 'path'
 import winston from 'winston'
 import winstonDaily from 'winston-daily-rotate-file'
-import { LOG_DIR } from '@/config/env'
+import { LOG_DIR } from '@/config'
 
 const logDir: string = join(__dirname, LOG_DIR)
 
