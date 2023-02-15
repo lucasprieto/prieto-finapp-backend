b15b785a8f2cbbc65d0f8adb74f75b8f1bce2ec2
Lucas Prieto <lucas.prieto@globant.com>
2023-02-15 13:00:11 -0300

    test swc

--- src/databases/mongo.ts
+++ src/databases/mongo.ts
@@ -1,4 +1,4 @@
-import { DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD } from '@/config'
+import { DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD } from '@/config/env'
 import { ConnectOptions } from 'mongoose'
 
 export const dbUri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`
